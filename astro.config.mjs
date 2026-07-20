7f9a5afa2e23851f2a085a27f78a530e6c175def
EXXodos911 <115137895+EXXodos911@users.noreply.github.com>
2026-07-20 16:46:49 +0300

    Pages fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
-  site: "https://exxodos911.github.io",
-  base: "/EXXodos911.github.io",
+  site: "https://EXXodos911.github.io",
   trailingSlash: "always",
 });
